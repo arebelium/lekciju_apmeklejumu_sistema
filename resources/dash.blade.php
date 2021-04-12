0f871eb52be4617b2ac0c1f703be6de8c31cb744
justineliene <64956233+justineliene@users.noreply.github.com>
2021-04-12 15:22:02 +0300

    Add files via upload

---
+++ resources/dash.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="edit-page">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 text-center">
+                    <h1>Lektoru panelis</h1>
+                </div>
+                
+            </div>
+            
+        </div>
+    </section>
+
+@endsection
